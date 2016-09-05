49751a8e62becf13ffc26695a04fcc049e70acd3
Dylan Shackelford <djshackx@gmail.com>
2016-09-04 22:45:12 -0700

    Initial Commit

---
+++ musicShack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  musicShack
+//
+//  Created by Dylan Shackelford on 9/4/16.
+//  Copyright © 2016 Dylan Shackelford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ musicShack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  musicShack
+//
+//  Created by Dylan Shackelford on 9/4/16.
+//  Copyright © 2016 Dylan Shackelford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
